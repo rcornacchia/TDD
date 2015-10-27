9136f5e3100b7342e9bb91df93a629baab1bb4a4
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-10-26 21:29:55 -0400

    tweak

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+assert 'To-Do' in browser.title, "Browser title was " + browser.title
